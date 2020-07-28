390035e12ab5927bba5e4404d4de6854b92f8889
braya <braya@BRYEPZ>
2020-07-28 16:21:17 -0500

    Clases controlador

--- src/ec/edu/ups/controlador/ControladorBodega.java
+++ src/ec/edu/ups/controlador/ControladorBodega.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ec.edu.ups.modelo;
+package ec.edu.ups.controlador;
 
 /**
  *
  * @author braya
  */
-public class Cliente {
+public class ControladorBodega {
     
 }
--- src/ec/edu/ups/controlador/ControladorCliente.java
+++ src/ec/edu/ups/controlador/ControladorCliente.java
@@ -1,14 +1,14 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ec.edu.ups.modelo;
+package ec.edu.ups.controlador;
 
 /**
  *
  * @author braya
  */
-public class Cliente {
+public class ControladorCliente {
     
 }
