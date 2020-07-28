64b3eda7ec5eb8ff476536b4ee6eac3bdde3f124
braya <braya@BRYEPZ>
2020-07-28 18:19:25 -0500

    Metodos en la clase constructor

--- src/ec/edu/ups/controlador/ControladorCliente.java
+++ src/ec/edu/ups/controlador/ControladorCliente.java
@@ -5,10 +5,38 @@
  */
 package ec.edu.ups.controlador;
 
+import ec.edu.ups.modelo.Producto;
+
 /**
  *
  * @author braya
  */
 public class ControladorCliente {
+    public double calcularSubtotal(){
+        return 0;
+    }
     
+    public double calcularTotal(){
+        return  0;
+    }
+    
+    public void añadirProducto(Producto producto){
+        //
+    }
+    
+    public void quitarProducto(Producto producto){
+        
+    }
+    
+    public void anularProducto(){
+        
+    }
+    
+    public void almacenar(Producto producto){
+        
+    }
+    
+    public Producto retirar(Producto producto){
+        return null;        
+    }
 }
