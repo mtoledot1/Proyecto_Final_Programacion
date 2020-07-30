825522f65802827e02f4dc26864798710bb84d64
LissethReinoso <LissethReinoso30@users.noreply.github.com>
2020-07-30 00:14:23 -0500

    "Añadiendo métodos a controladores"

---
+++ Desktop/b/Proyecto_Final_Programacion-Nicolas/src/ec/edu/ups/controlador/ControladorProducto.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.controlador;
+
+import ec.edu.ups.modelo.Producto;
+
+/**
+ *
+ * @author braya
+ */
+public class ControladorProducto {
+    
+    public void añadirProducto(Producto producto){
+        
+        //Dao
+    }
+    
+    public void quitarProducto(Producto producto){
+        //Dao
+    }
+    
+    public void anularProducto(){
+        
+    }
+    
+    public void almacenar(Producto producto){
+        
+    }
+    
+    public Producto retirar(Producto producto){
+        return null;        
+    }
+}
